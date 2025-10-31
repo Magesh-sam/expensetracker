ac97ee34766d42fb6f879bff3d3b6f6f09a910df
mageshkannan <mageshkannanam@gmail.com>
2025-10-31 11:45:51 +0530

    add: pojo: category, payment method, traction tracker

---
+++ src/model/pojo/PaymentMethod.java
@@ -0,0 +1,35 @@
+package model.pojo;
+
+public class PaymentMethod {
+    private int paymentMethodId;
+    private String name;
+
+    public PaymentMethod(int paymentMethodId, String name) {
+        this.paymentMethodId = paymentMethodId;
+        this.name = name;
+    }
+
+    public PaymentMethod(String name) {
+        this.name = name;
+    }
+
+    public PaymentMethod() {
+    }
+
+    public int getPaymentMethodId() {
+        return paymentMethodId;
+    }
+
+    public void setPaymentMethodId(int paymentMethodId) {
+        this.paymentMethodId = paymentMethodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/model/pojo/Transaction.java
@@ -0,0 +1,103 @@
+package model.pojo;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    public enum TransactionType {
+        expense, income
+    }
+
+    private int transactionId;
+    private int categoryId;
+    private int appuserId;
+    private int paymentMethodId;
+    private BigDecimal amount;
+    private TransactionType transactionType;
+    private LocalDateTime createdAt;
+
+    public Transaction(int transactionId, int categoryId, int appuserId, int paymentMethodId, BigDecimal amount,
+            TransactionType transactionType, LocalDateTime createdAt) {
+        this.transactionId = transactionId;
+        this.categoryId = categoryId;
+        this.appuserId = appuserId;
+        this.paymentMethodId = paymentMethodId;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
+    }
+
+    public Transaction(int categoryId, int appuserId, int paymentMethodId, BigDecimal amount,
+            TransactionType transactionType, LocalDateTime createdAt) {
+        this.categoryId = categoryId;
+        this.appuserId = appuserId;
+        this.paymentMethodId = paymentMethodId;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
+    }
+
+    public Transaction(BigDecimal amount, TransactionType transactionType, LocalDateTime createdAt) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getAppuserId() {
+        return appuserId;
+    }
+
+    public void setAppuserId(int appuserId) {
+        this.appuserId = appuserId;
+    }
+
+    public int getPaymentMethodId() {
+        return paymentMethodId;
+    }
+
+    public void setPaymentMethodId(int paymentMethodId) {
+        this.paymentMethodId = paymentMethodId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
