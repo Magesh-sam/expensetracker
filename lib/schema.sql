d15cb9770b3a84629ef779a8b619b1e00ae6eb05
mageshkannan <mageshkannanam@gmail.com>
2025-10-31 06:55:02 +0530

    update: schema

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,15 @@
+drop TABLE IF EXISTS app_user;
+
+drop TABLE IF EXISTS category;
+
+drop TABLE IF EXISTS transaction_tracker;
+
+drop TABLE IF EXISTS payment_method;
+
+drop TYPE if EXISTS transaction_type;
+
+ENUM transaction_type { expense, income }
+
 CREATE Table app_user (
     id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255),
@@ -16,10 +28,24 @@ CREATE Table transaction_tracker (
     category_id INTEGER REFERENCES category (id),
     app_user_id INTEGER REFERENCES app_users (id),
     payment_method_id INTEGER REFERENCES payment_method (id),
-    created_at TIMESTAMP
+    transaction_type VARCHAR(255),
+    created_at TIMESTAMP,
 );
 
 create Table payment_method (
     id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255)
 );
+
+INSERT INTO
+    payment_method (name)
+VALUES ('Cash'),
+    ('Credit Card'),
+    ('Debit Card');
+
+INSERT INTO
+    category (name)
+VALUES ('Groceries'),
+    ('Restaurants'),
+    ('Entertainment'),
+    ('Other');
